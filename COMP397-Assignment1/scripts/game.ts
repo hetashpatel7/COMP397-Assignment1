6ae968e7b90220b6c4a6080fccf7f7cf792784f6
hetashpatel7 <hetashpatel7@gmail.com>
2015-05-27 23:36:47 -0400

    Using Vox fonts

--- COMP397-Assignment1/scripts/game.ts
+++ COMP397-Assignment1/scripts/game.ts
@@ -1,4 +1,8 @@
 ﻿/// <reference path="typings/impress/impress.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
 
 
 
